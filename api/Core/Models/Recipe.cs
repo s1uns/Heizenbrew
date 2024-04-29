26ee9b9557243b3384c27ab30864383445d6f765
Illia Teliuk <illiateliuk@gmail.com>
2024-04-29 20:43:21 +0300

    Added some dtos

--- api/Core/Models/Recipe.cs
+++ api/Core/Models/Recipe.cs
@@ -5,7 +5,7 @@ namespace Core.Models
     public class Recipe : BaseEntity
     {
         public Brewer Brewer { get; set; }
-        public string Name { get; set; }
+        public string Title { get; set; }
         public string Description { get; set; }
         public ICollection<RecipeIngredient> Ingredients { get; set; }
 
---
+++ api/Infrustructure/Dto/Recipe/RecipeDto.cs
@@ -0,0 +1,17 @@
+﻿using Infrustructure.Dto.Ingredient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrustructure.Dto.Recipe
+{
+    public record RecipeDto
+    (
+        Guid Id,
+        string Title,
+        string Description,
+        IList<RecipeIngredientDto> Ingredients
+    );
+}
